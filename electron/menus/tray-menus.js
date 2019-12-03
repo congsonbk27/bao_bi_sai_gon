d34d8127c145868c54f6df429caa4470dce46b37
congsonbk27 <congsonbk27@gmail.com>
2019-12-03 22:56:00 +0700

    delete all

--- electron/menus/tray-menus.js
+++
@@ -1,9 +0,0 @@
-const { createWindow } = require('../window')
-
-const trayMenus = [
-  { label: 'Cân', click: () => createWindow('home') },
-  { label: 'Về chúng tôi', click: () => createWindow('about') },
-  { label: 'Thoát', role: 'quit' }
-]
-
-module.exports = trayMenus
